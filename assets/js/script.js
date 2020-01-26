f5dfe83c6299e5a373fe3bd816418bc0164bc40b
Maxime-Cao <maxime.cao20@gmail.com>
2020-01-26 15:07:18 +0100

    add anim reveal

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,24 @@ function showLetters() {
         clearTimeout(timeOut);
     }
 }
+const ratio = .3;
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .3
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+document.querySelectorAll('.reveal').forEach(r => {
+    observer.observe(r);
+});
