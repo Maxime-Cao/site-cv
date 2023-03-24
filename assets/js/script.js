2572c7fbb72077617d29eb8d3c82703ad8b63fb8
Maxime-Cao <m.cao@student.helmo.be>
2023-03-25 00:39:15 +0100

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,38 @@
-(function () {
-    'use strict';
+const navElems = [document.getElementById("firstNavElem"),document.getElementById("secondNavElem"),document.getElementById("thirdNavElem"),document.getElementById("fourthNavElem")];
 
-    const navElems = [document.getElementById("firstNavElem"),document.getElementById("secondNavElem"),document.getElementById("thirdNavElem"),document.getElementById("fourthNavElem")];
+const section1Position = document.getElementById("apropos").getBoundingClientRect().top + window.scrollY;
 
-    const section1Position = document.getElementById("apropos").getBoundingClientRect().top + window.scrollY;
+const section2Position = document.getElementById("competences").getBoundingClientRect().top + window.scrollY;
 
-    const section2Position = document.getElementById("competences").getBoundingClientRect().top + window.scrollY;
+const section3Position = document.getElementById("portfolio").getBoundingClientRect().top + window.scrollY;
 
-    const section3Position = document.getElementById("portfolio").getBoundingClientRect().top + window.scrollY;
+const section4Position = document.getElementById("contact").getBoundingClientRect().top + window.scrollY;
 
-    const section4Position = document.getElementById("contact").getBoundingClientRect().top + window.scrollY;
+const headerOffsetHeight = document.getElementById("header").offsetHeight;
 
-    const headerOffsetHeight = document.getElementById("header").offsetHeight;
+window.onscroll = function () {
+    let scrollPosition = document.documentElement.scrollTop;
 
-    window.onscroll = function () {
-        let scrollPosition = document.documentElement.scrollTop;
+    let scrollPositionWithHeader = scrollPosition + headerOffsetHeight;
 
-        let scrollPositionWithHeader = scrollPosition + headerOffsetHeight;
+    if(scrollPosition > 0) {
+        document.querySelector("#button").style.opacity = "1";
+    } else {
+        document.querySelector("#button").style.opacity = "0";
+    }
 
-        if(scrollPosition > 0) {
-            document.querySelector("#button").style.opacity = "1";
-        } else {
-            document.querySelector("#button").style.opacity = "0";
-        }
-
-        if(scrollPositionWithHeader >= section4Position) {
-            setSelectedSection(document.getElementById("fourthNavElem"));
-        } else if(scrollPositionWithHeader >= section3Position) {
-            setSelectedSection(document.getElementById("thirdNavElem"));
-        } else if(scrollPositionWithHeader >= section2Position) {
-            setSelectedSection(document.getElementById("secondNavElem"));
-        } else if(scrollPositionWithHeader >= section1Position) {
-            setSelectedSection(document.getElementById("firstNavElem"));
-        } else {
-            setSelectedSection(null);
-        }
-    };
-})();
+    if(scrollPositionWithHeader >= section4Position) {
+        setSelectedSection(document.getElementById("fourthNavElem"));
+    } else if(scrollPositionWithHeader >= section3Position) {
+        setSelectedSection(document.getElementById("thirdNavElem"));
+    } else if(scrollPositionWithHeader >= section2Position) {
+        setSelectedSection(document.getElementById("secondNavElem"));
+    } else if(scrollPositionWithHeader >= section1Position) {
+        setSelectedSection(document.getElementById("firstNavElem"));
+    } else {
+        setSelectedSection(null);
+    }
+};
 
 let setSelectedSection = (elemToSelect) => {
     if(elemToSelect != null) {
