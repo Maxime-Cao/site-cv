53e741ffbacad793ca178cb7c46caade49fef0b1
Maxime-Cao <m.cao@student.helmo.be>
2023-03-24 23:44:16 +0100

    refactor nav elems selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,71 @@
 (function () {
     'use strict';
 
-    var section = document.querySelectorAll(".section");
-    var sections = {};
-    var i = 0;
+    const section1 = document.getElementById("apropos");
+    const section1Position = section1.getBoundingClientRect().top + window.scrollY;
 
-    Array.prototype.forEach.call(section, function (e) {
-        sections[e.id] = e.offsetTop;
-    });
+    const section2 = document.getElementById("competences");
+    const section2Position = section2.getBoundingClientRect().top + window.scrollY;
+
+
+    const section3 = document.getElementById("portfolio");
+    const section3Position = section3.getBoundingClientRect().top + window.scrollY;
+
+    const section4 = document.getElementById("contact");
+    const section4Position = section4.getBoundingClientRect().top + window.scrollY;
+
+    const headerHeight = document.getElementById("header").offsetHeight;
+
 
     window.onscroll = function () {
-        var heightHeader = document.querySelector('#header').offsetHeight;
-        var scrollPosition = (document.documentElement.scrollTop + heightHeader) || (document.body.scrollTop + heightHeader);
-        var heightAccueil = document.querySelector('#accueil').offsetHeight;
+        let scrollPosition = document.documentElement.scrollTop;
 
-        for (i in sections) {
+        if(scrollPosition > 0) {
+            document.querySelector("#button").style.opacity = "1";
+        } else {
+            document.querySelector("#button").style.opacity = "0";
+        }
 
-            if (scrollPosition > heightHeader) {
-                document.querySelector("#button").style.opacity = "1";
-            } else {
-                document.querySelector("#button").style.opacity = "0";
+        switch(scrollPosition + offsetHeight) {
+            case section1Position: {
+                setSelectedSection(document.getElementById("firstNavElem"));
+                break;
             }
 
-            if (sections[i] <= scrollPosition) {
-                document.querySelector('.active').setAttribute('class', ' ');
-                document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+            case section2Position: {
+                setSelectedSection(document.getElementById("secondNavElem"));
+                break;
+            }
 
-            } else if (scrollPosition <= heightAccueil) {
-                document.querySelector('a[href*=' + i + ']').setAttribute('class', ' ');
+            case section3Position: {
+                setSelectedSection(document.getElementById("thirdNavElem"));
+                break;
+            }
+
+            case section4Position: {
+                setSelectedSection(document.getElementById("fourthNavElem"));
+                break;
+            }
+            default: {
+                setSelectedSection(null);
+                break;
             }
         }
+
+
+
     };
 })();
+
+let setSelectedSection = (sectionToSelect) => {
+    sectionToSelect.classList.add("active");
+
+    document.querySelectorAll(".section").forEach(section => {
+        if(section != sectionToSelect) {
+            section.classList.remove("active");
+        }
+    })
+}
 
 const ratio = .3;
 const options = {
